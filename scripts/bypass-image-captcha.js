41dd5d88f44cbb01696bff6c8ee0a8dfefe65964
Daniel <42862105+9hitste@users.noreply.github.com>
2022-07-19 16:18:49 +0700

    oups

--- scripts/bypass-image-captcha.js
+++ scripts/bypass-image-captcha.js
@@ -1,9 +1,10 @@
 /*
-To run this demo, please use this URL: https://www.google.com/recaptcha/api2/demo
+To run this demo, please use this URL:
+https://captcha.com/demos/features/captcha-demo.aspx
 Also replace YOUR_API_KEY with your 2captcha/anti-captcha API key
 See demo: https://youtu.be/v-6c6ckUNpE
 */
 
 await WaitForLoading ();
-await SolveRecaptcha("2captcha", "YOUR_API_KEY"); //you may use "anti-captcha" instead of "2captcha"
-await ClickBySelector('#recaptcha-demo-submit'); //click submit button
+await SolveImageCaptcha("#demoCaptcha_CaptchaImage", "#captchaCode", "2captcha", "YOUR_API_KEY"); //you may use "anti-captcha" instead of "2captcha"
+await ClickBySelector ("#validateCaptchaButton"); //click the validate button
